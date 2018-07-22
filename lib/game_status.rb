66cd2fdfb9b64bc5eb96a0b3c02554baaecea4a0
Beatrice Mendoza <mendoza@nyu.edu>
2018-07-22 22:40:18 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,15 +16,9 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
-    win_index_1  = win_combination[0]
-    win_index_2  = win_combination[1]
-    win_index_3  = win_combination[2]
-
-    if (position_taken?(board, win_index_1) && position_taken?(board, win_index_2) && position_taken?(board, win_index_3))
-      if board[win_index_1] == board[win_index_2] && board[win_index_2] == board[win_index_3]
-        return win_combination
-      end
+  WIN_COMBINATIONS.select do |win_combination|
+    if board[win_combination[0]] == board[win_combination[1]] && board[win_combination[1]] == board[win_combination[2]] && position_taken?(board, win_combination[0]) && position_taken?(board, win_combination[1]) && position_taken?(board, win_combination[2])
+      return win_combination
     end
   end
   return false
